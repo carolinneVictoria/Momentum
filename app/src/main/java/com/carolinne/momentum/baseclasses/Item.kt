51123acff724a023f70b992b343e0bb8d268dafc
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-06-12 19:09:14 -0300

    Update Item.kt

--- app/src/main/java/com/carolinne/momentum/baseclasses/Item.kt
+++ app/src/main/java/com/carolinne/momentum/baseclasses/Item.kt
@@ -1,8 +1,10 @@
 package com.carolinne.momentum.baseclasses
 
 data class Item(
+    var endereco: String? = null,
     var tarefa: String? = null,
     var statusTarefa: String? = null,
     val base64Image: String? = null,
     val imageUrl: String? = null
+
 )
