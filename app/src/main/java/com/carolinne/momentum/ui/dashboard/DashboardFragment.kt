17822668cf4399f94bc42fb2ee59c419b1b31d3e
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-06-12 18:53:33 -0300

    Update DashboardFragment.kt

--- app/src/main/java/com/carolinne/momentum/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/carolinne/momentum/ui/dashboard/DashboardFragment.kt
@@ -24,9 +24,7 @@ import com.carolinne.momentum.baseclasses.Item
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
-import kotlinx.coroutines.CoroutineStart
 
 
 class DashboardFragment : Fragment() {
